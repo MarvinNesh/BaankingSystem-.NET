8e9a866451a692b021b43a4cb559788f87e7c663
vhuhwavho <vhnetshjhb024@student.wethinkcode.co.za>
2025-10-18 16:07:42 +0200

    added a registration method

--- Bank.cs
+++ Bank.cs
@@ -5,7 +5,16 @@ namespace BankingSystem
 {
     public class Bank
     {
-    
+        private List<User> users = new List<User>();
+
+        public void RegisterUser(string name, string email, string password)
+        {
+            //  Create user and add to list
+            var user = new User(name, email);
+            user.SetPassword(password);
+            users.Add(user);
+            Console.WriteLine($"Registered user: {name}");
+        }
 
       
     }
