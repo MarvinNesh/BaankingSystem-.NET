c0509805cb6146f8e0ff3e3892a535bf1a266ec4
vhuhwavho <vhnetshjhb024@student.wethinkcode.co.za>
2025-10-18 09:47:29 +0200

    Implement basic password setter

--- User.cs
+++ User.cs
@@ -6,5 +6,14 @@ namespace BankingSystem
     {
         public string Name { get; private set; }
         public string Email { get; private set; }
+        public string Password { get; private set; } // TODO hash this
+
+
+        public void SetPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentException("Password cannot be empty.");
+            Password = password; //TODO hash the password when you store
+        }
     }
 }
