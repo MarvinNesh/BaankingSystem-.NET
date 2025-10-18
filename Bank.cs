9885c32ba2863347250d082abc774d1db59ab43c
vhuhwavho <vhnetshjhb024@student.wethinkcode.co.za>
2025-10-18 16:09:22 +0200

    added a login method

--- Bank.cs
+++ Bank.cs
@@ -16,6 +16,15 @@ namespace BankingSystem
             Console.WriteLine($"Registered user: {name}");
         }
 
-      
+        public User Login(string email, string password)
+        {
+            //  Find user and verify password
+            var user = users.Find(u => u.Email == email);
+            if (user != null && password == user.Password) // Plain check for now (hashed later)
+            {
+                return user;
+            }
+            throw new InvalidOperationException("Invalid credentials.");
+        }
     }
 }
