e2b4b4c643b0b9d8e4224a0c3edbfc9441b37e0d
vhuhwavho <vhnetshjhb024@student.wethinkcode.co.za>
2025-10-18 13:25:56 +0200

    Add temporary test for User in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace BankingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testing user
+            Console.WriteLine("Testing User class...");
+            try
+            {
+                var user = new User("Marvin Nesh", "marvin@test.com");
+                user.SetPassword("secret123");
+                Console.WriteLine($"User: {user.Name}");
+                Console.WriteLine($"Email: {user.Email}");
+                Console.WriteLine($"Hashed Password: {user.Password}"); 
+                Console.WriteLine("User test passed! Press Enter to exit.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Test failed: {ex.Message}");
+            }
+            Console.ReadLine();
+        }dotnet build
+dotnet run
+    }
+}
