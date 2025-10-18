7394ce7f4ec18e21879669daea76f8242af2606c
vhuhwavho <vhnetshjhb024@student.wethinkcode.co.za>
2025-10-18 22:19:20 +0200

    Add test: User constructor throws on invalid email

---
+++ BankingSystem.Tests/UserTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using BankingSystem;
+
+namespace BankingSystem.Tests
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Constructor_WithValidData_SetsProperties()
+        {
+            var user = new User("Nesh Marvin", "marvin@test.com");
+
+            Assert.Equal("Nesh Marvin", user.Name);
+            Assert.Equal("marvin@test.com", user.Email);
+        }
+
+        [Fact]
+        public void Constructor_WithNullName_ThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new User(null, "test@test.com"));
+            Assert.Equal("name", ex.ParamName);
+        }
+
+        [Fact]
+        public void Constructor_WithInvalidEmail_ThrowsArgumentException()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new User("Marvin", ""));
+            Assert.Contains("Invalid email format", ex.Message);
+        }
+    }
+}
