a0737a17196479012237a72b05783ff37c3b81bb
vhuhwavho <vhnetshjhb024@student.wethinkcode.co.za>
2025-10-19 03:20:03 +0200

    Add test: Account Withdraw with insufficient funds throws

--- BankingSystem.Tests/AccountTests.cs
+++ BankingSystem.Tests/AccountTests.cs
@@ -3,14 +3,13 @@ using BankingSystem;
 
 namespace BankingSystem.Tests
 {
-    
     public class TestAccount : Account
     {
         public TestAccount(string ownerName, decimal initialBalance = 0) : base(ownerName, initialBalance) { }
 
         public override decimal CalculateInterest()
         {
-            return 0m; 
+            return 0m;
         }
     }
 
@@ -19,23 +18,18 @@ namespace BankingSystem.Tests
         [Fact]
         public void Deposit_WithValidAmount_IncreasesBalance()
         {
-            
             var account = new TestAccount("Nesh Marvin", 100m);
 
-            
             account.Deposit(50m);
 
-            
             Assert.Equal(150m, account.Balance);
         }
 
         [Fact]
         public void Deposit_WithInvalidAmount_ThrowsArgumentException()
         {
-            
             var account = new TestAccount("Nesh Marvin", 100m);
 
-            
             var ex = Assert.Throws<ArgumentException>(() => account.Deposit(-50m));
             Assert.Contains("Deposit amount must be positive", ex.Message);
         }
@@ -43,14 +37,20 @@ namespace BankingSystem.Tests
         [Fact]
         public void Withdraw_WithValidAmount_DecreasesBalance()
         {
-            
             var account = new TestAccount("Nesh Marvin", 100m);
 
-            
             account.Withdraw(50m);
 
-        
             Assert.Equal(50m, account.Balance);
+        }
+
+        [Fact]
+        public void Withdraw_WithInsufficientFunds_ThrowsInvalidOperationException()
+        {
+            var account = new TestAccount("Nesh Marvin", 100m);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => account.Withdraw(150m));
+            Assert.Contains("Insufficient funds", ex.Message);
         }
     }
 }
