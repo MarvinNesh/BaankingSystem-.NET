fd3ccf0532ecafd0ec1c700a6de341c328ae7e36
vhuhwavho <vhnetshjhb024@student.wethinkcode.co.za>
2025-10-19 03:40:30 +0200

    Add test: Account CalculateInterest abstract method throws

--- BankingSystem.Tests/AccountTests.cs
+++ BankingSystem.Tests/AccountTests.cs
@@ -76,5 +76,28 @@ namespace BankingSystem.Tests
             Assert.NotEmpty(account.AccountNumber);
             Assert.Contains("-", account.AccountNumber);
         }
+        // Minimal stub for testing abstract method (does not override)
+public class MinimalTestAccount : Account
+{
+    public MinimalTestAccount(string ownerName, decimal initialBalance = 0) : base(ownerName, initialBalance) { }
+
+    public override decimal CalculateInterest()
+    {
+        throw new NotSupportedException("Interest calculation is not supported for this account type.");
+    }
+}
+
+
+
+[Fact]
+public void CalculateInterest_AbstractMethod_ThrowsNotSupportedException()
+{
+    
+    var account = new MinimalTestAccount("Nesh Marvin", 100m);
+
+
+    var ex = Assert.Throws<NotSupportedException>(() => account.CalculateInterest());
+    Assert.Contains("not supported", ex.Message.ToLower());  
+}
     }
 }
